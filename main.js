dda58053c55eb4e0c1263566af05a628cde6d49c
Muneeb <thinkmuneeb@gmail.com>
2021-07-30 11:24:36 +0500

    upadte, delete commands in mongodb

--- main.js
+++ main.js
@@ -9,17 +9,29 @@ const Todo = require("./models/todo");
 
 const init = async () => {
   mongoose.connect("mongodb://localhost/berry-db");
-  // await create();
   await read();
-};
-
-const create = async () => {
-  const response = await Todo.create({ record: "Muneeb 1" });
-  console.log({ response });
+  // await create();
+  // await update();
+  // await deleteRecord();
 };
 
 const read = async () => {
   const response = await Todo.find({});
+  console.log({ response });
+};
+
+const create = async () => {
+  const response = await Todo.create({ record: "Muneeb Khan", date: 200 });
+  console.log({ response });
+};
+
+const update = async () => {
+  const response = await Todo.updateOne({ record: "mzk" }, { record: "mzk.." });
+  console.log({ response });
+};
+
+const deleteRecord = async () => {
+  const response = await Todo.deleteOne({ record: "mzk.." });
   console.log({ response });
 };
 
