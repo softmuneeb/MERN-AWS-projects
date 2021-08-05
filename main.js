a2ee4626d81f3f52e9fb2fb7381002bd12c354e8
Muneeb <thinkmuneeb@gmail.com>
2021-08-05 15:35:20 +0500

    testing

--- main.js
+++ main.js
@@ -1,38 +1,12 @@
-const mongoose = require("mongoose");
-const Todo = require("./models/todo");
-
-// tutorial https://www.youtube.com/watch?v=5QEwqX5U_2M
-
-// first install mongo db and robo T3
-// https://robomongo.org/download
-// https://www.mongodb.com/try/download/community
+const HTMLtoDOCX = require("html-to-docx");
 
 const init = async () => {
-  mongoose.connect("mongodb://localhost/berry-db");
-  await read();
-  // await create();
-  // await update();
-  // await deleteRecord();
-};
-
-const read = async () => {
-  const response = await Todo.find({});
-  console.log({ response });
-};
-
-const create = async () => {
-  const response = await Todo.create({ record: "Muneeb Khan", date: 200 });
-  console.log({ response });
-};
-
-const update = async () => {
-  const response = await Todo.updateOne({ record: "mzk" }, { record: "mzk.." });
-  console.log({ response });
-};
-
-const deleteRecord = async () => {
-  const response = await Todo.deleteOne({ record: "mzk.." });
-  console.log({ response });
+  const a = await HTMLtoDOCX(
+    '<html><body><divclass="container"><h1>404</h1></div></body></html>'
+  );
+  const fs = require("fs");
+  await fs.writeFile("filename1.docx", a);
+  console.log("a: ", a);
 };
 
 init();
