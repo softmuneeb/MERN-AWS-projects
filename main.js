49b91ce995b5810934bbdd24fca95eb7b1c6221a
Muneeb <thinkmuneeb@gmail.com>
2021-08-08 12:20:52 +0500

    date()

--- main.js
+++ main.js
@@ -1,38 +1,6 @@
-const mongoose = require("mongoose");
-const Todo = require("./models/todo");
-
-// tutorial https://www.youtube.com/watch?v=5QEwqX5U_2M
-
-// first install mongo db and robo T3
-// https://robomongo.org/download
-// https://www.mongodb.com/try/download/community
-
 const init = async () => {
-  mongoose.connect("mongodb://localhost/berry-db");
-  await read();
-  // await create();
-  // await update();
-  // await deleteRecord();
+  console.log(new Date() === new Date(Date.now()-3));
+  console.log("new Date(): ", new Date());
+  console.log("new Date(Date.now()): ", new Date(Date.now()));
 };
-
-const read = async () => {
-  const response = await Todo.find({});
-  console.log({ response });
-};
-
-const create = async () => {
-  const response = await Todo.create({ record: "Muneeb Khan", date: 200 });
-  console.log({ response });
-};
-
-const update = async () => {
-  const response = await Todo.updateOne({ record: "mzk" }, { record: "mzk.." });
-  console.log({ response });
-};
-
-const deleteRecord = async () => {
-  const response = await Todo.deleteOne({ record: "mzk.." });
-  console.log({ response });
-};
-
 init();
