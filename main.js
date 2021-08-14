efb6c48e5c94a758bf1b68a3baad7114437b56b5
Muneeb <thinkmuneeb@gmail.com>
2021-08-14 14:30:36 +0500

    good

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-const fun = (a, b) => a + b;
+const fun1 = (a, b) => a + b;
+const fun2 = (a, b) => {
+  return a + b;
+};
 
-const funny = (a) => (b) => a + b;
+const funny1 = (a) => (b) => a + b;
+const funny2 = (a) => (b) => {
+  return a + b;
+};
 
 const moreFunny = (a) => {
   return (b) => {
@@ -9,8 +15,12 @@ const moreFunny = (a) => {
 };
 
 const init = async () => {
-  console.log(fun(1, 2));
-  console.log(funny(1)(2));
+  console.log(fun1(1, 2));
+  console.log(fun2(1, 2));
+
+  console.log(funny1(1)(2));
+  console.log(funny2(1)(2));
+
   console.log(moreFunny(1)(2));
 };
 init();
