21fae5b7bc4750ef99189ab5a5d0d9bf6808584a
Muneeb <thinkmuneeb@gmail.com>
2021-08-17 16:13:25 +0500

    cron job

--- main.js
+++ main.js
@@ -1,26 +1,11 @@
-const fun1 = (a, b) => a + b;
-const fun2 = (a, b) => {
-  return a + b;
-};
-
-const funny1 = (a) => (b) => a + b;
-const funny2 = (a) => (b) => {
-  return a + b;
-};
-
-const moreFunny = (a) => {
-  return (b) => {
-    return a + b;
-  };
-};
-
-const init = async () => {
-  console.log(fun1(1, 2));
-  console.log(fun2(1, 2));
-
-  console.log(funny1(1)(2));
-  console.log(funny2(1)(2));
-
-  console.log(moreFunny(1)(2));
-};
-init();
+var CronJob = require('cron').CronJob;
+var job = new CronJob(
+  '* * * * * *',
+  function () {
+    console.log('You will see this message every second');
+  },
+  null,
+  true,
+  'America/Los_Angeles'
+);
+job.start();
