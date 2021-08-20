db917a4cfd297445d7da3642698394e6bb4d8c1d
Muneeb <thinkmuneeb@gmail.com>
2021-08-20 13:11:16 +0500

    working multicall ALHAMDULILLAH

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
+// nodeUrl: 'https://rinkeby.infura.io/v3/3da1c863472e43d989856450d4e6889d',
+// multicallCustomContractAddress: '0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696'
+
+import { Multicall } from 'ethereum-multicall';
 import Web3 from 'web3';
+import { busdAbi, busdAddress } from './smart-contracts.js';
 
 const init = async () => {
   const web3 = new Web3(
     'https://rinkeby.infura.io/v3/3da1c863472e43d989856450d4e6889d'
   );
 
-  const res = await web3.eth.getBalance(
-    '0x960b236A07cf122663c4303350609A66A7B288C0'
-  );
-  console.log('res: ', res);
+  const multicall = new Multicall({ web3Instance: web3, tryAggregate: true });
+
+  const contractCallContext = [
+    {
+      reference: 'testContract',
+      contractAddress: busdAddress,
+      abi: busdAbi,
+      calls: [
+        {
+          reference: 'fooCall1',
+          methodName: 'balanceOf',
+          methodParameters: [busdAddress]
+        },
+        {
+          reference: 'fooCall2',
+          methodName: 'balanceOf',
+          methodParameters: [busdAddress]
+        }
+      ]
+    }
+  ];
+
+  const results = await multicall.call(contractCallContext);
+
+  console.log(results.results.testContract.callsReturnContext);
 };
+
+// getPresaleDetails(addresses);
 
 init();
