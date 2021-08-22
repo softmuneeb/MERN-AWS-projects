18c4e6f422212f42fe99a4ad591dd53169842dff
mudaseriqbalshah <mudaseriqbalshah@gmail.com>
2021-08-22 15:15:18 +0500

    successful

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 import BigNumber from 'bignumber.js';
 import pkg from 'web3-utils';
-const { toWei } = pkg;
+
+const { toWei, fromWei } = pkg;
 
 const init = async () => {
-  const _someBusdCoins = toWei('1000');
+  const _someBusdCoins = toWei('4000');
   // get 5000 Coins Sum
 
   const num_someBusdCoins = Number(_someBusdCoins) / 10;
   console.log(
     'num_someBusdCoins: ',
-    num_someBusdCoins + num_someBusdCoins + num_someBusdCoins,
+    num_someBusdCoins + num_someBusdCoins + num_someBusdCoins
   );
 
   const bigNum_someBusdCoins = BigNumber(_someBusdCoins) / 10;
-  console.log(
-    'bigNum_someBusdCoins: ',
-    bigNum_someBusdCoins + bigNum_someBusdCoins + bigNum_someBusdCoins,
-  );
+  const sum =
+    bigNum_someBusdCoins + bigNum_someBusdCoins + bigNum_someBusdCoins;
+  console.log('bigNum_someBusdCoins: ', new BigNumber('4e+21') / 10 ** 18);
 
   // new BigNumber();
 };
