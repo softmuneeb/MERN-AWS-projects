33e8e65538ec69f52e34ff8291e7c6ecf01153b0
Muneeb <thinkmuneeb@gmail.com>
2021-08-22 12:59:50 +0500

    working fine

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-const init = async () => {};
+import BigNumber from 'bignumber.js';
+
+const init = async () => {
+  const num = Number(100000000000000000000);
+  console.log(
+    '21 Decimals is limit of Number datatype in JS:  ',
+    num.toString(),
+  );
+
+  const bigNum = BigNumber(100000000000000000000);
+  console.log('bigNum: ', bigNum.toString());
+
+  // new BigNumber();
+};
 
 init();
