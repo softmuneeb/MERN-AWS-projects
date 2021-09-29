6d4d7088d90a5ed29f5f413f9c758da13bd2464b
Muneeb <thinkmuneeb@gmail.com>
2021-09-29 19:34:08 +0500

    init

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-const HTMLtoDOCX = require("html-to-docx");
-
-const init = async () => {
-  const a = await HTMLtoDOCX(
-    '<html><body><divclass="container"><h1>404</h1></div></body></html>'
-  );
-  const fs = require("fs");
-  await fs.writeFile("filename1.docx", a);
-  console.log("a: ", a);
-};
-
-init();
+console.log('each branch is a small project');
