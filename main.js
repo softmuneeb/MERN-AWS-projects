3bef459020d4ad6d3f7f09c942633860c8990ae4
Muneeb <thinkmuneeb@gmail.com>
2021-09-30 00:52:26 +0500

    working yuhoo

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 const fs = require('fs');
+const find = 'trait-type';
+const replace = 'trait_type';
+const searchInFolder = './someFolder/';
 
 const init = async () => {
   try {
-    const files = await fs.promises.readdir('.');
+    const files = await fs.promises.readdir(searchInFolder);
     for (const file of files) {
-      fs.rename(file, file.replace('.json', ''), function (err) {
-        if (err) console.log('ERROR: ' + err.message + err);
-      });
+      console.log('file: ', file);
+      try {
+        const content = fs.readFileSync(searchInFolder + file, 'utf8');
+        const fixedContent = content.split(find).join(replace);
+        fs.writeFile(searchInFolder + file, fixedContent, () => {});
+      } catch (e) {
+        console.error('whoops ', e.message);
+      }
     }
   } catch (e) {
     console.error("We've thrown! Whoops!", e.message);
