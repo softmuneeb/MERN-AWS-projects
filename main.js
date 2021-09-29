5392b4322d49667418f1cd17dd5bf880c8bd917a
Muneeb <thinkmuneeb@gmail.com>
2021-09-29 19:39:35 +0500

    read files in current dir

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('each branch is a small project');
+const fs = require('fs');
+
+const init = async () => {
+  try {
+    const files = await fs.promises.readdir('.');
+    for (const file of files) {
+      console.log('file: ', file);
+    }
+  } catch (e) {
+    console.error("We've thrown! Whoops!", e.message);
+  }
+};
+
+init();
