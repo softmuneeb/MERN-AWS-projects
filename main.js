1a47fec56f962cbe24eef851a931fd49eb06eb7b
Muneeb <thinkmuneeb@gmail.com>
2021-10-02 03:17:06 +0500

    ok

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const searchInFolder = './someFolder/';
+const file = 'bg1.png';
+
+import fs from 'fs';
 import * as IPFS from 'ipfs-core';
 import pinataSDK from '@pinata/sdk';
 import { PINATA_API_KEY, PINATA_API_SECRET } from './secret.js';
@@ -9,11 +13,13 @@ const handleCode = async func => {
     console.log('e: ', e.message.substring(0, 100));
   }
 };
-
+// https://gist.github.com/MarkoCen/0ee9437439e00e313926
 (async () => {
   handleCode(async () => {
+    const content = fs.readFile(searchInFolder + file, 'base64');
+
     const ipfs = await IPFS.create();
-    let { cid } = await ipfs.add('Alhamdulliah');
+    let { cid } = await ipfs.add(content);
     const hash = cid + '';
 
     const pinata = pinataSDK(PINATA_API_KEY, PINATA_API_SECRET);
