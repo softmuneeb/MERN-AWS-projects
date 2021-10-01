e28ecefd7e722987d2f4e3eaddc330ff2179f139
Muneeb <thinkmuneeb@gmail.com>
2021-10-02 02:39:49 +0500

    work init, do not work here, its only template

--- Readme.md
+++ Readme.md
@@ -1,5 +1,11 @@
 ### How to run code
 
+yarn
+
+yarn start
+
+### Use passwords, api keys etc like this
+
 Use your api keys, make copy of file secret2.js and name it secret.js
 
 put your api key here for pinata
--- main.js
+++ main.js
@@ -1,24 +1,3 @@
-import * as IPFS from 'ipfs-core';
-import pinataSDK from '@pinata/sdk';
-import { PINATA_API_KEY, PINATA_API_SECRET } from './secret.js';
+// do not work here, its only template
 
-const handleCode = async func => {
-  try {
-    await func();
-  } catch (e) {
-    console.log('e: ', e);
-  }
-};
-
-(async () => {
-  const ipfs = await IPFS.create();
-  const { cid } = await ipfs.add('Hello world');
-  console.log('\n\n\n');
-  console.log(cid);
-
-  // handleCode(async () => {
-  //   const pinata = pinataSDK(PINATA_API_KEY, PINATA_API_SECRET);
-  //   await pinata.testAuthentication();
-  //   await pinata.pinByHash('QmNRCQWfgze6AbBCaT1rkrkV5tJ2aP4oTNPb5JZcXYywve');
-  // });
-})();
+(async () => {})();
