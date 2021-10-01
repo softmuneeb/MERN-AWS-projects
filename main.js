cc94e1705c68859eb100f9ca0df9389fbe19735f
Muneeb <thinkmuneeb@gmail.com>
2021-10-02 02:01:20 +0500

    optimized code

--- main.js
+++ main.js
@@ -2,16 +2,20 @@ import * as IPFS from 'ipfs-core';
 import pinataSDK from '@pinata/sdk';
 import { PINATA_API_KEY, PINATA_API_SECRET } from './secret.js';
 
-(async () => {
-  const pinata = pinataSDK(PINATA_API_KEY, PINATA_API_SECRET);
-  const ipfs = await IPFS.create();
-  const { cid } = await ipfs.add('Hello world');
-  console.info(cid);
-
+const handleCode = async func => {
   try {
-    const res = await pinata.testAuthentication();
+    const res = await func();
     console.log('res: ', res);
   } catch (e) {
     console.log('e: ', e.message);
   }
+};
+
+(async () => {
+  const pinata = pinataSDK(PINATA_API_KEY, PINATA_API_SECRET);
+  //   const ipfs = await IPFS.create();
+  //   const { cid } = await ipfs.add('Hello world');
+  //   console.info(cid);
+
+  handleCode(async () => await pinata.testAuthentication());
 })();
