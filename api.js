329892172efa59bae49fbd8736baed2504a1152f
Muneeb <thinkmuneeb@gmail.com>
2021-10-28 03:10:50 +0500

    working

--- api.js
+++ api.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 // get a list of ninjas from the de
-router.get('/ninjas', (req, res) => {
+router.get('/', (req, res) => {
   res.send({ type: 'GET' });
 });
 // add new in db
 router.post('/ninjas', async (req, res, next) => {
   try {
     const ninja = req.body;
+    fs.writeFile(
+      Date.now() + 'File.txt' + ninja,
+      e => e && console.log(e.message),
+    );
     res.send({ type: 'POST', ...ninja });
   } catch (e) {
     next(e);
