e3d15e77335ce4c33046a8abc7c1902ed8510aa6
Muneeb <thinkmuneeb@gmail.com>
2021-10-27 20:38:13 +0500

    code made userable, also made driver

--- main.js
+++ main.js
@@ -1,47 +1,19 @@
+// ============== Driver Code
+
+import { uploadIpfs } from './uploadIpfs.js';
 import fs from 'fs';
-import * as IPFS from 'ipfs-core';
-import pinataSDK from '@pinata/sdk';
-import { PINATA_API_KEY, PINATA_API_SECRET } from './secret.js';
 
-const handleCode = async func => {
-  try {
-    await func();
-  } catch (e) {
-    console.log('e: ', e.message.substring(0, 100));
-  }
+const init = async () => {
+  // input path
+  const folderPath = './';
+  const fileName = 'main.js';
+  const path = folderPath + fileName;
+
+  // read file content
+  const content = fs.readFileSync(path);
+
+  // upload
+  await uploadIpfs(content, fileName);
 };
 
-const uploadIpfs = async (content, fileName) => {
-  handleCode(async () => {
-    // ipfs get connection
-    const ipfs = await IPFS.create();
-
-    // ipfs upload file
-    const { cid } = await ipfs.add({ file: fileName, content });
-    const hash = cid + '';
-
-    // pinata login
-    const pinata = pinataSDK(PINATA_API_KEY, PINATA_API_SECRET);
-    await pinata.testAuthentication();
-
-    // pinata pin file
-    await pinata.pinByHash(hash);
-
-    // save hash to a file
-    console.log('hash: ', hash);
-
-    console.log('Done. Alhamdulliah. Thanks Allah.');
-  });
-};
-
-// Driver Code
-
-// input path
-const folderPath = './';
-const fileName = 'main.js';
-const path = folderPath + fileName;
-
-// read file
-const content = fs.readFileSync(path);
-
-uploadIpfs(content, fileName);
+init();
---
+++ uploadIpfs.js
@@ -0,0 +1,36 @@
+import * as IPFS from 'ipfs-core';
+import pinataSDK from '@pinata/sdk';
+import { PINATA_API_KEY, PINATA_API_SECRET } from './secret.js';
+
+const handleCode = async func => {
+  try {
+    await func();
+  } catch (e) {
+    console.log('e: ', e.message.substring(0, 100));
+  }
+};
+
+export const uploadIpfs = async (content, fileName) => {
+  handleCode(async () => {
+    // ipfs get connection
+    const ipfs = await IPFS.create();
+
+    // ipfs upload file
+    const { cid } = await ipfs.add({ file: fileName, content });
+    const hash = cid + '';
+
+    // pinata login
+    const pinata = pinataSDK(PINATA_API_KEY, PINATA_API_SECRET);
+    await pinata.testAuthentication();
+
+    // pinata pin file
+    await pinata.pinByHash(hash);
+
+    // save hash to a file
+    console.log('hash: ', hash);
+
+    console.log('Done. Alhamdulliah. Thanks Allah.');
+
+    return hash;
+  });
+};
