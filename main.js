8d3816f44725e7471e185e25aaabb9301d2e11ee
Muneeb <thinkmuneeb@gmail.com>
2021-10-28 14:36:44 +0500

    ok

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = require('./api');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express(); //set up express
 require('dotenv').config();
