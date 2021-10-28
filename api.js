424e96fcd4cf824553ba819b1e7e75174bdeaabe
Muneeb <thinkmuneeb@gmail.com>
2021-10-28 13:48:28 +0500

    metadata zip download worked

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
+const zipFolder = require('zip-folder');
 // get a list of ninjas from the de
 router.get('/', (req, res) => {
   res.send({ type: 'GET' });
@@ -19,5 +20,16 @@ router.post('/ninjas', async (req, res, next) => {
   } catch (e) {
     console.log('e: ', e.message);
   }
+
+  //zipFolder
+});
+
+router.get('/zip', async (req, res) => {
+  zipFolder('./metadata', './metadata.zip', err => {
+    if (err) console.log(err.message);
+    else {
+      res.download('./metadata.zip');
+    }
+  });
 });
 module.exports = router;
