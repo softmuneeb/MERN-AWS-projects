87533ae631378c1c0b39443e049976dab46e0a4b
Muneeb <thinkmuneeb@gmail.com>
2021-11-05 22:45:10 +0500

    ok

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
+// dependencies
 const axios = require('axios');
 
+// config
+const nft = '0x18f87c05325ae47bfe75c039198b3dc1cb2ed23d';
+const tokenIdFrom = 1;
+const tokenIdTo = 1;
+
+// run code
 const init = async () => {
-  for (let i = 0; i < 10000; ++i) {
+  for (let i = tokenIdFrom; i <= tokenIdTo; i++) {
     const res = await axios.get(
-      'https://api.opensea.io/api/v1/asset/0x350b4cdd07cc5836e30086b993d27983465ec014/1/?force_update=true',
+      `https://api.opensea.io/api/v1/asset/${nft}/1/?force_update=true`,
     );
 
-    console.log('res.data.image_url.length: ', res.data.image_url.length);
     if (res.data.image_url.length < 5) console.log('not ', i);
-    else if (i % 1000 === 0) console.log('ok ', i);
+    else if (i % 1 === 0) console.log('ok ', i);
   }
 };
 
