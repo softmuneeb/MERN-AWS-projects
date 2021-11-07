71523bba1239374262d5e62164aed032bcb3c28f
Muneeb <thinkmuneeb@gmail.com>
2021-11-07 19:20:04 +0500

    ok

--- main.js
+++ main.js
@@ -1,20 +1,14 @@
-// dependencies
-const axios = require('axios');
+let csvToJson = require('convert-csv-to-json');
+const fs = require('fs');
+const csvtojsonV2 = require('csvtojson');
+let fileInputName = 'NFT.csv';
+let fileOutputName = 'myOutputFile.json';
 
-// config
-const nft = '0x18f87c05325ae47bfe75c039198b3dc1cb2ed23d';
-const tokenIdFrom = 1;
-const tokenIdTo = 1;
-
-// run code
 const init = async () => {
-  for (let i = tokenIdFrom; i <= tokenIdTo; i++) {
-    const url = `https://api.opensea.io/api/v1/asset/${nft}/${i}/?force_update=true`;
-    const res = await axios.get(url);
-
-    if (res.data.image_url.length < 5) console.log('not ', i);
-    else if (i % 1 === 0) console.log('ok ', i);
-  }
+  fs.writeFile(
+    fileOutputName,
+    JSON.stringify(await csvtojsonV2().fromFile(fileInputName)),
+    e => e && console.log(e),
+  );
 };
-
 init();
