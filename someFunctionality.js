70dd8fd19e70503f84ae7f2f29e02a032515097d
Muneeb <thinkmuneeb@gmail.com>
2021-11-10 05:37:56 +0500

    run ok

--- someFunctionality.js
+++ someFunctionality.js
@@ -1,3 +1,7 @@
+import linkPreviewGenerator from 'link-preview-generator';
+
 export const someFunctionality = async () => {
   console.log('Assalamo Alaikum');
+  const previewData = await linkPreviewGenerator('https://serene-mclean-ce533e.netlify.app/');
+  console.log(previewData);
 };
