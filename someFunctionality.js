cfdd98c9eed3272f9c49aa8b9bfd0679d67591a9
Muneeb <thinkmuneeb@gmail.com>
2021-11-10 05:56:57 +0500

    working

--- someFunctionality.js
+++ someFunctionality.js
@@ -1,7 +1,30 @@
-import linkPreviewGenerator from 'link-preview-generator';
+import { getLinkPreview, getPreviewFromContent } from 'link-preview-js';
 
 export const someFunctionality = async () => {
   console.log('Assalamo Alaikum');
-  const previewData = await linkPreviewGenerator('http://localhost:3000/');
-  console.log(previewData);
+
+  // pass the link directly
+  getLinkPreview('https://serene-mclean-ce533e.netlify.app/').then(data =>
+    console.debug(data),
+  );
+  // pass the link directly
+  getLinkPreview('https://www.youtube.com/').then(data => console.debug(data));
+
+  ////////////////////////// OR //////////////////////////
+
+  // pass a pre-fetched response object
+  // The passed response object should include, at minimum:
+  // {
+  //   data: '<!DOCTYPE...><html>...',     // response content
+  //   headers: {
+  //     ...
+  //     // should include content-type
+  //     content-type: "text/html; charset=ISO-8859-1",
+  //     ...
+  //   },
+  //   url: 'https://domain.com/'          // resolved url
+  // }
+  // yourAjaxCall(url, response => {
+  //   getPreviewFromContent(response).then(data => console.debug(data));
+  // });
 };
