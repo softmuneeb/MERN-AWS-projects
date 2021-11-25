aee9c6fc844cb085fe875dd4f982921ef1cd4bde
Muneeb <thinkmuneeb@gmail.com>
2021-11-25 13:17:54 +0500

    bird nft

--- refreshMetadata.js
+++ refreshMetadata.js
@@ -2,27 +2,19 @@
 const axios = require('axios');
 
 // config
-const nft = '0x18f87c05325ae47bfe75c039198b3dc1cb2ed23d';
-const tokenIdFrom = 8000;
-const tokenIdTo = 8009;
+const nft = '0x1d4f5FBC869fDefd2b6D0e20C9c679E03E4CFF2F';
+const tokenIdFrom = 1;
+const tokenIdTo = 107;
 
 // run code
 const init = async () => {
-  let j = 0;
   for (let i = tokenIdFrom; i <= tokenIdTo; i++) {
     const url = `https://api.opensea.io/api/v1/asset/${nft}/${i}/?force_update=true`;
+    console.log(`url: ${url}`);
+    const res = await axios.get(url);
 
-    setTimeout(() => {
-      axios
-        .get(url)
-        .then(res => {
-          if (res.data.image_url.length < 5) console.log('not ', i);
-          else if (i % 1 === 0) console.log('ok ', i);
-        })
-        .catch(e => {
-          e && console.log(e.message);
-        });
-    }, 200 * j++);
+    if (res.data.image_url.length < 5) console.log('not ', i);
+    else if (i % 1 === 0) console.log('ok ', i);
   }
 };
 
