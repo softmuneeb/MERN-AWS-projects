c72d243144fb826659cf8597412a282de32fef22
Abraham Peter <abraham.peter.hash@gmail.com>
2021-12-09 17:46:49 +0500

    working okay

--- api.js
+++ api.js
@@ -1,38 +1,41 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fs = require('fs');
-const zipFolder = require('zip-folder');
+const fs = require("fs");
+const zipFolder = require("zip-folder");
 // get a list of ninjas from the de
-router.get('/', (req, res) => {
-  res.send({ type: 'GET' });
+router.get("/", (req, res) => {
+  res.send({ type: "GET" });
 });
 // add new in db
-router.post('/ninjas', async (req, res, next) => {
-  console.log('req: ', req);
-  console.log('req: ', req.body);
+router.post("/ninjas", async (req, res, next) => {
+  if (process.env.JWT === null || process.env.JWT === undefined) {
+    res.send({ message: "JWT not set" });
+  }
+  if (req.body.JWT !== process.env.JWT) {
+    res.send({ message: "Wrong JWT" });
+    return;
+  }
+  console.log("req: ", req);
+  console.log("req: ", req.body);
   try {
     fs.writeFile(
-      "./metadata/" +
-        JSON.parse(req.body.metadata).tokenId +
-        "__" +
-        Date.now() +
-        ".json",
-      JSON.stringify(JSON.parse(req.body.metadata), null, 4),
+      "./metadata/" + req.body.metadata.tokenId + "__" + Date.now() + ".json",
+      JSON.stringify(req.body.metadata, null, 4),
       (e) => e && console.log(e.message)
     );
-    res.send({ type: 'POST' });
+    res.send({ type: "POST" });
   } catch (e) {
-    console.log('e: ', e.message);
+    console.log("e: ", e.message);
   }
 
   //zipFolder
 });
 
-router.get('/zip', async (req, res) => {
-  zipFolder('./metadata', './metadata.zip', err => {
+router.get("/zip", async (req, res) => {
+  zipFolder("./metadata", "./metadata.zip", (err) => {
     if (err) console.log(err.message);
     else {
-      res.download('./metadata.zip');
+      res.download("./metadata.zip");
     }
   });
 });
