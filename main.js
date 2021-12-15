db6f4bc4920cee0b71d7a761264425c1e1e84288
Muneeb <thinkmuneeb@gmail.com>
2021-12-16 04:03:39 +0500

    limit increased

--- main.js
+++ main.js
@@ -6,8 +6,13 @@ require('dotenv').config();
 
 const port = process.env.PORT || 4000;
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb' }));
+const fs = require('fs');
+
+fs.rename('penguinsUsed/88', 'penguinsUsed/88_', e => e && console.log(e));
+fs.rename('penguinsUsed/93', 'penguinsUsed/93_', e => e && console.log(e));
+
+app.use(express.json({ limit: '2mb' }));
+app.use(express.urlencoded({ limit: '2mb' }));
 app.use(cors());
 app.use(express.json());
 app.use('/api', router);
