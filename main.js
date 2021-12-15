37003bf66bcb788fcc649e639e0de862211f2865
Muneeb <thinkmuneeb@gmail.com>
2021-12-16 03:25:29 +0500

    ok good limit increased

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ require('dotenv').config();
 
 const port = process.env.PORT || 4000;
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 app.use(cors());
 app.use(express.json());
 app.use('/api', router);
