c1c2fa445e796de42dab6cb1bda9940cd433af1a
Muneeb <thinkmuneeb@gmail.com>
2021-12-16 04:05:26 +0500

    fs rename

--- main.js
+++ main.js
@@ -6,10 +6,9 @@ require('dotenv').config();
 
 const port = process.env.PORT || 4000;
 
-const fs = require('fs');
-
-fs.rename('penguinsUsed/88', 'penguinsUsed/88_', e => e && console.log(e));
-fs.rename('penguinsUsed/93', 'penguinsUsed/93_', e => e && console.log(e));
+// const fs = require('fs');
+// fs.rename('penguinsUsed/88', 'penguinsUsed/88_', e => e && console.log(e));
+// fs.rename('penguinsUsed/93', 'penguinsUsed/93_', e => e && console.log(e));
 
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ limit: '2mb' }));
