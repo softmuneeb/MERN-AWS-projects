9a3a80fb42088f56fc24b1ae0195d997ba717953
Muneeb <muneeb.zubair.hash@gmail.com>
2022-01-02 22:46:37 +0500

    ok

--- api.js
+++ api.js
@@ -8,23 +8,6 @@ router.get("/", (req, res) => {
   res.send({ type: "GET" });
 });
 
-router.get("/is-sardine-available/:tokenId", async (req, res) => {
-  const tokenId = Number(req.params.tokenId);
-  if (isNaN(tokenId) || tokenId <= 0 || tokenId >= 10000) {
-    res.send({ success: false, message: "token id invalid" });
-    return;
-  }
-
-  fs.access(`./sardinesUsed/${req.params.tokenId}`, fs.F_OK, (e) =>
-    e
-      ? res.send({ success: true })
-      : res.send({
-          success: false,
-          message: "token id used for penguin merge",
-        }),
-  );
-});
-
 router.get("/is-penguin-available/:tokenId", async (req, res) => {
   try {
     const tokenId = Number(req.params.tokenId);
@@ -57,15 +40,6 @@ router.post("/ninjas", async (req, res, next) => {
       return;
     }
     console.log(JSON.stringify(req.body, null, 4));
-    const sardineTokenId = Number(req.body.metadata.sardineTokenIdUsed);
-    if (
-      isNaN(sardineTokenId) ||
-      sardineTokenId <= 0 ||
-      sardineTokenId >= 10000
-    ) {
-      res.send({ success: false, message: "sardine token id invalid" });
-      return;
-    }
 
     const penguinTokenId = Number(req.body.metadata.tokenId);
     if (
@@ -77,66 +51,44 @@ router.post("/ninjas", async (req, res, next) => {
       return;
     }
 
-    console.log(`sardineTokenIdUsed: ${sardineTokenId}`);
-    console.log(`penguinTokenId: ${penguinTokenId}\n`);
-
-    const path = `./penguinsUsed/${sardineTokenId}`;
-    const sardinePath = `./sardinesUsed/${sardineTokenId}`;
+    const path = `./penguinsUsed/${penguinTokenId}`;
 
     fs.access(path, fs.F_OK, (e) => {
       // file not exists = penguin available for penguin merge
       if (e) {
-        fs.access(sardinePath, fs.F_OK, (e) => {
-          if (e) {
-            // file not exists = sardine available for penguin merge
-            try {
-              fs.writeFile(
-                `./sardinesUsed/${req.body.metadata.sardineTokenIdUsed}`,
-                "",
-                (e) => e && console.log(e.message),
-              ); // now sardine is not available penguin merge
+        try {
+          fs.writeFile(
+            `./penguinsUsed/${req.body.metadata.tokenId}`,
+            "",
+            (e) => e && console.log(e.message),
+          ); // now penguin is not available penguin merge
 
-              fs.writeFile(
-                `./penguinsUsed/${req.body.metadata.tokenId}`,
-                "",
-                (e) => e && console.log(e.message),
-              ); // now penguin is not available penguin merge
+          fs.writeFile(
+            `./penguinsUsed/${req.body.metadata.tokenIdBurned}`,
+            "",
+            (e) => e && console.log(e.message),
+          ); // now penguin burned is not available penguin merge
 
-              fs.writeFile(
-                `./penguinsUsed/${req.body.metadata.tokenIdBurned}`,
-                "",
-                (e) => e && console.log(e.message),
-              ); // now penguin burned is not available penguin merge
+          fs.writeFile(
+            `./metadata/${req.body.metadata.tokenId}.png`,
+            req.body.image.split(";base64,").pop(),
+            { encoding: "base64" },
+            (e) => e && console.log(e.message),
+          );
 
-              fs.writeFile(
-                `./metadata/${req.body.metadata.tokenId}.png`,
-                req.body.image.split(";base64,").pop(),
-                { encoding: "base64" },
-                (e) => e && console.log(e.message),
-              );
+          fs.writeFile(
+            `./metadata/${req.body.metadata.tokenId}`,
+            JSON.stringify(req.body.metadata, null, 4),
+            (e) => e && console.log(e.message),
+          ); // saving metadata of merged penguin
 
-              fs.writeFile(
-                `./metadata/${req.body.metadata.tokenId}`,
-                JSON.stringify(req.body.metadata, null, 4),
-                (e) => e && console.log(e.message),
-              ); // saving metadata of merged penguin
-
-              res.send({ success: true });
-              return;
-            } catch (e) {
-              (e) => e && console.log(e.message);
-              res.send({ success: false, message: "error occured" });
-              return;
-            }
-          } else {
-            // file exists = sardine used for penguin merge
-            res.send({
-              success: false,
-              message: "token id used for penguin merge",
-            });
-            return;
-          }
-        });
+          res.send({ success: true });
+          return;
+        } catch (e) {
+          (e) => e && console.log(e.message);
+          res.send({ success: false, message: "error occured" });
+          return;
+        }
       } else {
         // file exists = sardine used for penguin merge
         res.send({
@@ -186,27 +138,11 @@ router.get("/zip", async (req, res) => {
   });
 });
 
-router.get("/zipSardinesUsed", async (req, res) => {
-  zipFolder("./sardinesUsed", "./sardinesUsed.zip", (err) => {
-    if (err) console.log(err.message);
-    else {
-      res.download("./sardinesUsed.zip");
-    }
-  });
-});
 router.get("/zipError", async (req, res) => {
   zipFolder("./error", "./error.zip", (err) => {
     if (err) console.log(err.message);
     else {
       res.download("./error.zip");
-    }
-  });
-});
-router.get("/zipPenguinImages", async (req, res) => {
-  zipFolder("./metadata", "./metadata.zip", (err) => {
-    if (err) console.log(err.message);
-    else {
-      res.download("./metadata.zip");
     }
   });
 });
