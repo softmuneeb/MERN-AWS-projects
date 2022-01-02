509c9fe35a0fac6e828b54f2e4f9d27a79486623
Muneeb <muneeb.zubair.hash@gmail.com>
2022-01-02 19:45:35 +0500

    init

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 const HTMLtoDOCX = require("html-to-docx");
 
+/*
+get rows of csv as objs
+for each obj make a new file
+
+open all files, use script in img-gen, img-gen new, bird branch
+add image
+add name
+add description
+*/
+import csvtojson from 'csvtojson';
+
 const init = async () => {
-  const a = await HTMLtoDOCX(
-    '<html><body><divclass="container"><h1>404</h1></div></body></html>'
-  );
-  const fs = require("fs");
-  await fs.writeFile("filename1.docx", a);
-  console.log("a: ", a);
+  let finderObjs = await csvtojson().fromFile(fileInput1);
 };
 
 init();
