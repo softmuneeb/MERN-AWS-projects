ad9bdfc74ed86e75591c9c5f024af330af1d01b4
Muneeb <muneeb.zubair.hash@gmail.com>
2022-01-03 11:01:09 +0500

    working to generate 1 metadata

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const HTMLtoDOCX = require("html-to-docx");
-
 /*
 get rows of csv as objs
 for each obj make a new file
@@ -9,10 +7,90 @@ add image
 add name
 add description
 */
-import csvtojson from 'csvtojson';
+import csvtojson from "csvtojson";
+import fs from "fs";
+
+const getMetadataFromObj = (obj) => {
+  let attributes = [];
+
+  // get each trait
+  const traits = Object.keys(obj);
+  traits.map((trait) => {
+    if (
+      trait !== "tokenId" &&
+      trait !== "tokenIdBurned" &&
+      obj[trait] !== undefined &&
+      obj[trait] !== null
+    ) {
+      attributes.push({ trait_type: trait, value: obj[trait] });
+    }
+  });
+
+  // remove spaces and comma at end of attributes
+  const metadata = {
+    attributes,
+  };
+
+  return JSON.stringify(metadata, null, 4);
+};
+
+// Snow Doodle #10.png ===>>> 10
+const see = (f) => f.replace("Snow Doodle #", "").replace(".png", "");
 
 const init = async () => {
-  let finderObjs = await csvtojson().fromFile(fileInput1);
+  let rows = await csvtojson().fromFile("SnowiesMetadata.csv");
+
+  let row = rows[0];
+  let { Background, Body, Head, Hair, Face, Neck } = row;
+  let obj = { Background, Body, Head, Hair, Face, Neck };
+  let metadata = getMetadataFromObj(obj);
+  fs.writeFile(
+    `output/${see(row["File Name"])}`,
+    metadata,
+    (e) => e && console.log(e),
+  );
+
+  console.log(metadata);
 };
 
 init();
+
+/*
+{
+  'File Name': 'Snow Doodle #1.png',
+  Background: 'Orange 1',
+  Body: 'Skinny Green 1',
+  Head: 'Snowman',
+  Hair: 'Grease 3',
+  Face: 'Saddie 1',
+  Neck: 'Warm Orange 1'
+}
+{
+    "attributes": [
+        {
+            "trait_type": "Background",
+            "value": "Orange 1"
+        },
+        {
+            "trait_type": "Body",
+            "value": "Skinny Green 1"
+        },
+        {
+            "trait_type": "Head",
+            "value": "Snowman"
+        },
+        {
+            "trait_type": "Hair",
+            "value": "Grease 3"
+        },
+        {
+            "trait_type": "Face",
+            "value": "Saddie 1"
+        },
+        {
+            "trait_type": "Neck",
+            "value": "Warm Orange 1"
+        }
+    ]
+}
+*/
