a9291116805233d7d3f631cc40adb6ce4751a4b1
Muneeb <muneeb.zubair.hash@gmail.com>
2022-01-09 21:56:05 +0500

    better code

--- addNumberToFiles.js
+++ addNumberToFiles.js
@@ -1,28 +1,16 @@
-const fs = require('fs');
-const searchInFolder = './someFolder/';
+import { rename, promises } from "fs";
 
-const init = async () => {
-  try {
-    const files = await fs.promises.readdir(searchInFolder);
-    for (let file of files) {
-      console.log('file: ', file);
-      try {
-        try {
-          let filen = (Number(file.replace('.png', '')) + 479) % 9999;
-          
-          fs.copyFile(
-            searchInFolder + file,
-            searchInFolder + 'new/' + filen + '.png',
-            e => e && console.log(e.message)
-          );
-        } catch (error) {}
-      } catch (e) {
-        console.error('whoops ', e.message);
-      }
-    }
-  } catch (e) {
-    console.error("We've thrown! Whoops!", e.message);
-  }
+const someThing = (searchInFolder, file) => {
+  let fileNumber = Number(file.replace(".png", "")) + 0;
+  if (isNaN(fileNumber)) console.log({ file });
+  rename(
+    searchInFolder + file,
+    searchInFolder + fileNumber + ".png",
+    (e) => e && console.log(e.message),
+  );
 };
 
-init();
+export const addNumberToFiles = async (searchInFolder) => {
+  const files = await promises.readdir(searchInFolder);
+  for (let file of files) someThing(searchInFolder, file);
+};
