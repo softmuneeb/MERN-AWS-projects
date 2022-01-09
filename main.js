33435627955eb77f9c631004c5dd8be1f977b9f4
Muneeb <muneeb.zubair.hash@gmail.com>
2022-01-09 21:42:23 +0500

    working for snowies special images 30 snow doodle special #3.png -> 3.png

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
-const dir = "./snowies 10,000/";
+const dir = "./snowies 30/";
 const err = (e) => e && console.log(e.message);
 
 const init = async () => {
   const files = await fs.promises.readdir(dir);
   console.log("files ", files[0]);
   for (const file of files) {
-    fs.rename(dir + file, dir + file.replace("Snow Doodle #", ""), err);
+    fs.rename(dir + file, dir + file.replace("Snowies Limited #", ""), err);
     // break;
   }
 };
