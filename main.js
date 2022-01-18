3820a4a650e16e599650c89faf9b8fae881e33d5
Muneeb <muneeb.zubair.hash@gmail.com>
2022-01-18 20:55:37 +0500

    done

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
-const dir = "./snowies 30/";
+const dir = "./images";
 const err = (e) => e && console.log(e.message);
 
 const init = async () => {
   const files = await fs.promises.readdir(dir);
   console.log("files ", files[0]);
+
   for (const file of files) {
-    fs.rename(dir + file, dir + file.replace("Snowies Limited #", ""), err);
-    // break;
+    console.log(file);
   }
 };
 
