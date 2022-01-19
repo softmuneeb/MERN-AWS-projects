4cbacd3c1821d3b0fba35887013317b0d5617d62
Muneeb <muneeb.zubair.hash@gmail.com>
2022-01-20 02:12:50 +0500

    ok

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
+// read traits.json file and add some N to all token ids
+
 import fs from "fs";
 
+const N = 100;
+
 const init = async () => {
-  let json = fs.readFileSync("traits.json", 'utf8');
-  console.log({ json: json[0].Background });
+  let tokens = JSON.parse(fs.readFileSync("traits.json", "utf8"));
+  // console.log({ json: json[0].Background });
+
+  tokens = tokens.map((token) => ({
+    ...token,
+    tokenId: Number(token.tokenId) + N,
+  }));
+  console.log(JSON.stringify(tokens, null, 4));
 };
 
 init();
