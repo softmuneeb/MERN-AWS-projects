576570221adb256a20fe8def9f65fd8f26037bda
Muneeb <muneeb.zubair.hash@gmail.com>
2022-01-20 01:56:25 +0500

    ok

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
-const HTMLtoDOCX = require("html-to-docx");
+// const HTMLtoDOCX = require("html-to-docx");
 
 const init = async () => {
-  const a = await HTMLtoDOCX(
-    '<html><body><divclass="container"><h1>404</h1></div></body></html>'
-  );
-  const fs = require("fs");
-  await fs.writeFile("filename1.docx", a);
-  console.log("a: ", a);
+  fs.readFileSync()
 };
 
 init();
