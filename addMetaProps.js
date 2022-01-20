04bbbd955dbd48ecc716d008b2ae4fe7314c1f51
Muneeb <muneeb.zubair.hash@gmail.com>
2022-01-20 13:24:13 +0500

    meta gen

--- addMetaProps.js
+++ addMetaProps.js
@@ -1,37 +1,47 @@
 import { promises, readFileSync, writeFile } from "fs";
-const searchInFolder = "./metadata/";
-const imagesFolderHash = "QmazfNQJ6Cnx6SvWgKEN4ZwgQKncGCmsHRBY1uzJfSZEFH";
-const tokenName = "Snow Doodle";
-const description = "Unique doodle collectibles on Avax";
-const external_url = "https://snowiesclub.com/";
+const searchInFolder = "./metadataGenerated/";
+const imagesFolderHash = "QmX5rF55nRfzs8nEXA7wC2eREieHq5Xynh6kghyR6NfTau";
+const tokenName = "Cheeky Cougar";
+// const description = "Unique doodle collectibles on Avax";
+const external_url = "https://www.cheekycougarclub.com/";
 
 const init = async () => {
   const files = await promises.readdir(searchInFolder);
 
   for (const file of files) {
-    console.log('file ', file);
-    if(file === ".DS_Store") continue;
+    // console.log("file ", file);
+    if (file === ".DS_Store") continue;
+
     const filePath = searchInFolder + file;
     const content = readFileSync(filePath);
-    let metadata = JSON.parse(content);
 
-    // metadata.name = `${tokenName} #${file}`;
-    // metadata.image = `ipfs://${imagesFolderHash}/${file}.png`;
+    let metadata;
+    try {
+       metadata = JSON.parse(content);
+      
+    } catch (err) {
+      console.log({ filePath });;
+    }
+
+    metadata.name = `${tokenName} #${file}`;
+    metadata.image = `ipfs://${imagesFolderHash}/${file}.jpg`;
     // metadata.description = description;
-    // metadata.external_url = external_url;
-    metadata.attributes = [
-      ...metadata.attributes,
-      {
-        trait_type: "Merged",
-        value: "SZN1",
-      },
-    ];
+    metadata.external_url = external_url;
+    // console.log({ metadata });
+    // metadata.attributes = [
+    //   ...metadata.attributes,
+    //   {
+    //     trait_type: "Merged",
+    //     value: "SZN1",
+    //   },
+    // ];
 
     writeFile(
-      filePath,
+      "./metadataGenerated1/" + file,
       JSON.stringify(metadata, null, 4),
       (e) => e && console.log(e.message),
     );
+    // return;
   }
 };
 
