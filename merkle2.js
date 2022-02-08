8e9bf703e4cfb21416186d3e086ad5c4818c4239
Muneeb <muneeb.zubair.hash@gmail.com>
2022-02-08 23:31:26 +0500

    working locally for addresses

--- merkle2.js
+++ merkle2.js
@@ -2,13 +2,17 @@
 // MerkleTree https://www.youtube.com/watch?v=n6nEPaE7KZ8
 
 const { MerkleTree } = require("merkletreejs");
-const hash = require("crypto-js/sha256");
+const hash = require("keccak256");
 
-const leaves = ["a", "b", "c"].map((x) => hash(x));
+const leaves = [
+  "0xd9af96861de6992b299e9ac004aa4c68771d0815",
+  "0x70c2264d3672ec8efa89457317e51bad5e6b86fa",
+  "0x2a30aaa70ac67ae355708a5c77fbe62c9394b65b",
+].map((x) => hash(x));
 const tree = new MerkleTree(leaves, hash);
 
 const root = tree.getRoot(); // requirement 1
-const leaf = hash("a1"); // requirement 2
+const leaf = hash("0x71c2264d3672ec8efa89457317e51bad5e6b86fa"); // requirement 2
 const proof = tree.getProof(leaf); // requirement 3
 
 console.log(tree.verify(proof, leaf, root)); // true
