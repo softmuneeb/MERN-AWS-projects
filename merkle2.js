b81bf4ebd9e9e3d392c8d12344094b44a67cd871
Muneeb <muneeb.zubair.hash@gmail.com>
2022-02-08 17:05:55 +0500

    ok

--- merkle2.js
+++ merkle2.js
@@ -1,11 +1,14 @@
+// https://www.npmjs.com/package/merkletreejs
+// MerkleTree https://www.youtube.com/watch?v=n6nEPaE7KZ8
+
 const { MerkleTree } = require("merkletreejs");
 const hash = require("crypto-js/sha256");
 
 const leaves = ["a", "b", "c"].map((x) => hash(x));
 const tree = new MerkleTree(leaves, hash);
 
-const root = tree.getRoot();
-const leaf = hash("a1");
-const proof = tree.getProof(leaf);
+const root = tree.getRoot(); // requirement 1
+const leaf = hash("a1"); // requirement 2
+const proof = tree.getProof(leaf); // requirement 3
 
 console.log(tree.verify(proof, leaf, root)); // true
