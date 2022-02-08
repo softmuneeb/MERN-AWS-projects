75de0606946fc8bf6fd513dbdb95d9726e71370c
Muneeb <muneeb.zubair.hash@gmail.com>
2022-02-09 00:33:21 +0500

    working

--- merkle1.js
+++ merkle1.js
@@ -1,11 +1,17 @@
 const { MerkleTree } = require("merkletreejs");
 const keccak256 = require("keccak256");
+const { toChecksumAddress } = require("web3-utils");
 
 const elements = [
   "0xd9af96861de6992b299e9ac004aa4c68771d0815",
   "0x70c2264d3672ec8efa89457317e51bad5e6b86fa",
   "0x2a30aaa70ac67ae355708a5c77fbe62c9394b65b",
-];
+  "0x5B38da6a701c568545dCfcB03FcB875f56beddC4",
+  "0x2A30aaa70ac67ae355708a5c77fbe62c9394b65b",
+  "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2",
+  "0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db",
+].map((a) => toChecksumAddress(a));
+
 // console.log({ elements }); // ['A', 'B', 'C', 'D', ....]
 const merkleTree = new MerkleTree(elements, keccak256, {
   hashLeaves: true,
@@ -13,11 +19,11 @@ const merkleTree = new MerkleTree(elements, keccak256, {
 });
 
 const root = merkleTree.getHexRoot();
-console.log({ root }); // 0x4ead2a9bca1c784460fca2314d4a05298e50a65a1eff8ed5f3a048d74d17c1f7;
-const leaf = keccak256("0xd9af96861de6992b299e9ac004aa4c68771d0815");
+console.log({ root }); // 0xb0717f36e23963e162cb790edf4388d08bb3fba22a1efa4d2ea6286f8ce4fa79;
+const leaf = keccak256("0x5B38Da6a701c568545dCfcB03FcB875f56beddC4");
 // console.log({ leaf }); // <Buffer 03 78 3f ac 2e fe d8 fb...>
 const proof = merkleTree.getHexProof(leaf);
-// console.log({ proof });
+console.log(JSON.stringify(proof, null, 4));
 // proof: [
 //   "0x1f675bff07515f5df96737194ea945c36c41e7b4fcef307b7cd4d0e602a69111",
 //   "0xe62e1dfc08d58fd144947903447473a090c958fe34e2425d578237fcdf1ab5a4",
