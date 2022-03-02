86f4a9ac11fd7b29fec79db64ca5aca469131534
Muneeb <muneeb.zubair.hash@gmail.com>
2022-03-03 02:30:02 +0500

    2776 done

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // Code work: read traits.json file and add some N to all token ids
 // Run like: node main.js > traits-v1.json
 
-import { tokens_ } from "./_3_FemaleShirtPant.js";
+import { tokens_ } from "./_4_MaleShirt.js";
 
-const N = 871;
+const N = 2776;
 
 let tokens = tokens_.map((token) => ({
   ...token,
