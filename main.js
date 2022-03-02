04a3128e99b5ef05661a5261f0c6d6632c8484f5
Muneeb <muneeb.zubair.hash@gmail.com>
2022-03-03 02:50:42 +0500

    file add, done 0

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // Code work: read traits.json file and add some N to all token ids
 // Run like: node main.js > traits-v1.json
 
-import { tokens_ } from "./_1_bikniTopBottomMetaData.js";
+import { _tokens_ } from "./1traits.js";
 
 const N = 0;
 
-let tokens = tokens_.map((token) => ({
+let tokens = _tokens_.map((token) => ({
   ...token,
   tokenId: Number(token.tokenId) + N,
 }));
