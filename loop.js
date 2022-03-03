1c3809e01c02458aa05ca494b785474c9b798e0d
Muneeb <muneeb.zubair.hash@gmail.com>
2022-03-03 20:45:38 +0500

    ok

--- loop.js
+++ loop.js
@@ -1,9 +1,8 @@
 import { clothes_traits_all } from "./clothes_traits_all.js";
-let log = console.log;
 
 const filt_clothes_traits_all = clothes_traits_all.map((trait) => {
   delete trait.tokenId;
   return trait;
 });
 
-log(JSON.stringify(filt_clothes_traits_all, null, 4));
+console.log(JSON.stringify(filt_clothes_traits_all, null, 4));
