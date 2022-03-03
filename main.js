88df25b1238db1d9276a45dec3e226be061e9bf1
Muneeb <muneeb.zubair.hash@gmail.com>
2022-03-03 15:19:39 +0500

    add traits

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // code converts metadata i.e { clothes: 2 } -> { shirt: blue, pant: green}, i.e id to actual clothes
 
 // plan
-// traits file variable with all clothes info merged
+// get traits file variable with all clothes info merged
 // get all 11111 img traits file
 // for every 11111 img traits replace clothes number with ...obj of all cothes info
 // log this > traitsV1.json
