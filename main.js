a09080885a21b3781022079af6b77b2042aa00cd
Muneeb <muneeb.zubair.hash@gmail.com>
2022-03-03 17:18:57 +0500

    mid stash

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 // code converts metadata i.e { clothes: 2 } -> { shirt: blue, pant: green}, i.e id to actual clothes
 
+import { all_traits } from "./all_traits.js";
+
 // plan
 // get traits file variable with all clothes info merged
 // get all 11111 img traits file
 // for every 11111 img traits replace clothes number with ...obj of all cothes info
 // log this > traitsV1.json
 
-const init = async () => {
-  
-};
+all_traits[0]
+console.log( all_traits[0]  );
 
-init();
+// const init = async () => {
+// };
+
+// init();
