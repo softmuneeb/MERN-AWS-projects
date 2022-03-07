e8cad07ee5ee14866d110d69c2844786c71afeee
Muneeb <muneeb.zubair.hash@gmail.com>
2022-03-08 01:20:32 +0500

    2 sec to 5 sec delay in result

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { getPenguinTokensForAddress } from "./multicall.js";
 
 console.log(Date());
-await getPenguinTokensForAddress("0xc18e78c0f67a09ee43007579018b2db091116b4c");
+await getPenguinTokensForAddress("0xc18e78c0f67a09ee43007579018b2db091116b4c"); // 2 sec to 5 sec delay in result
 console.log(Date());
