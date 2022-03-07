f5145d44f67f984080683a8f7dc64262f2e72ffc
Muneeb <muneeb.zubair.hash@gmail.com>
2022-03-08 03:03:45 +0500

    working 1 to 3 seconds walletOfOwner

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import { getContractNft } from "./smart-contract.js";
 console.log(Date());
 // tru with bp comic
 // await getNftTokensForAddress("0xc18e78c0f67a09ee43007579018b2db091116b4c"); // 2 sec to 5 sec delay in result
-const res = await getContractNft().methods.isApprovedOrOwner("0xc18e78c0f67a09ee43007579018b2db091116b4c", 5).call();
+const res = await getContractNft().methods.walletOfOwner("0x29f9ef8286dcc4f9a94340278db01f12c3483988").call();
 console.log("res: ", res);
 
 console.log(Date());
