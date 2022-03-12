ca1ed4087c320adda1656f6fc1d4aa74a3782006
Muneeb <muneeb.zubair.hash@gmail.com>
2022-03-13 02:49:20 +0500

    step 1, get file names, run fine

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 // code converts metadata i.e { clothes: 2 } -> { shirt: blue, pant: green}, i.e id to actual clothes
 
 // plan
-// traits file variable with all clothes info merged
-// get all 11111 img traits file
-// for every 11111 img traits replace clothes number with ...obj of all cothes info
+// step 1, get file names, traits file variable with all clothes info merged
+// get all 10k imgs folder
+// for every img take out traits
 // log this > traitsV1.json
 
-const init = async () => {
-  
-};
+import { promises } from "fs";
 
-init();
+const files = await promises.readdir("./imagesForMetadata");
+
+for (const file of files) {
+    console.log({ file });
+
+}
