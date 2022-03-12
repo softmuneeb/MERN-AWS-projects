be10f4c4b538a9166bd4dfbecc9098ce20ec4d85
Muneeb <muneeb.zubair.hash@gmail.com>
2022-03-13 04:03:06 +0500

    correct variable names

--- genMetadata.js
+++ genMetadata.js
@@ -1,4 +1,13 @@
-// code converts metadata i.e { clothes: 2 } -> { shirt: blue, pant: green}, i.e id to actual clothes
+import { copyFile, promises, writeFile } from "fs";
+
+// code converts file name to metadata i.e makes { trait_type: "Color 2", value: "Copper" } from "basic_1_Caribbean Green_Copper_Deep Sea_Honey Flower_Endeavour_Husk_193321_i_0_(15_2p0_2p0_0p5_4_0p15_0p0_128_2_5_2_10L-(_2)_111415"
+// also this code adds ipfs image link to metadata
+
+// how to run it
+// open this folder in vs code
+// make 3 folders written below on l
+// in terminal: node genMetadata.js
+
 
 // plan
 // step 1, get file names, traits file variable with all clothes info merged
@@ -7,13 +16,12 @@
 // save in file
 // QmSLjHCZVF6fHT8PiEFW96X7nV8MMfMbMLJU5wFZt3P3QY
 
-import { copyFile, promises, writeFile } from "fs";
 
 const startTokenId = 1;
 const imgExtension = ".png";
-const ipfsImgFolder = "./imagesForMetadata/";
+const imagesFolder = "./imagesForMetadata/";
 const imgNumberedFolder = "./imagesNumbered/";
-const imagesFolder = "ipfs://QmSLjHCZVF6fHT8PiEFW96X7nV8MMfMbMLJU5wFZt3P3QY/";
+const ipfsImagesFolder = "ipfs://QmSLjHCZVF6fHT8PiEFW96X7nV8MMfMbMLJU5wFZt3P3QY/";
 
 const genMetadata = (file, i) => {
   let f = file.split("_");
@@ -26,10 +34,10 @@ const genMetadata = (file, i) => {
   attributes.push({ trait_type: "Color 4", value: f[5] });
   attributes.push({ trait_type: "Color 5", value: f[6] });
   attributes.push({ trait_type: "Color 6", value: f[7] });
-  writeFile("metadataFromImgFileNames/" + i, JSON.stringify({ image: imagesFolder + i, attributes }, null, 4), (e) => e && console.log(e));
-  copyFile(ipfsImgFolder + file, imgNumberedFolder + i + imgExtension, (e) => e && console.log(e));
+  writeFile("metadataFromImgFileNames/" + i, JSON.stringify({ image: ipfsImagesFolder + i, attributes }, null, 4), (e) => e && console.log(e));
+  copyFile(imagesFolder + file, imgNumberedFolder + i + imgExtension, (e) => e && console.log(e));
 };
 
-const files = await promises.readdir(ipfsImgFolder);
+const files = await promises.readdir(imagesFolder);
 
 files.map((file, i) => genMetadata(file, i + startTokenId));
