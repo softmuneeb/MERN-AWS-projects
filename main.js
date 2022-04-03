51a916110a7589078023b026740f4a830407429a
Muneeb <muneeb.zubair.hash@gmail.com>
2022-04-03 10:18:34 +0500

    better

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-// code converts metadata i.e { clothes: 2 } -> { shirt: blue, pant: green}, i.e id to actual clothes
+// what this code do? 
 
 // plan
-// traits file variable with all clothes info merged
-// get all 11111 img traits file
-// for every 11111 img traits replace clothes number with ...obj of all cothes info
-// log this > traitsV1.json
+//
 
 const init = async () => {
   
