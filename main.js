bef08ce2afe67c5965bf7a78f839b103f8e32625
Muneeb <muneeb.zubair.hash@gmail.com>
2022-04-12 11:59:43 +0500

    quiz 3

--- main.js
+++ main.js
@@ -6,11 +6,19 @@
 const init = async () => {
   // What is the output of the following code?
 
-let arr = [1, "Turing", {x : 2}, [3, 4]];
+  let arr = [1, "Turing", { x: 2 }, [3, 4]];
 
-delete arr[1];
+  console.log(arr.length);
+  delete arr[1];
 
-console.log(arr.length);
+  console.log(arr.length);
+  console.log(arr);
+
+  /*
+4
+4
+[ 1, <1 empty item>, { x: 2 }, [ 3, 4 ] ]
+  */
 };
 
 init();
