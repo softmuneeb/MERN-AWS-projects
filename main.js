7a81c98832d2ef382c0c6af36774edfeada333cc
Muneeb <muneeb.zubair.hash@gmail.com>
2022-04-12 11:58:44 +0500

    quiz 3

--- main.js
+++ main.js
@@ -4,14 +4,13 @@
 //
 
 const init = async () => {
-  let inputs = [2, "turing", 3,4,5,6,7]
-  for (let i = 0; i < inputs.length; i++) {
-    if (inputs[i] === "turing") 
-      inputs.splice(i, 1)
-    else console.log(inputs[i]);
-    
-    
-  }
+  // What is the output of the following code?
+
+let arr = [1, "Turing", {x : 2}, [3, 4]];
+
+delete arr[1];
+
+console.log(arr.length);
 };
 
 init();
