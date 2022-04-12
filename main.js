55d8d94241ba2d52590555893c5e6cbb1bdb81e1
Muneeb <muneeb.zubair.hash@gmail.com>
2022-04-12 11:46:53 +0500

    quiz 2

--- main.js
+++ main.js
@@ -4,9 +4,14 @@
 //
 
 const init = async () => {
-  let a = { y: 10 }
-  a.x = a
-  console.log(JSON.stringify(a));
+  let inputs = [2, "turing", 3,4,5,6,7]
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i] === "turing") 
+      inputs.splice(i, 1)
+    else console.log(inputs[i]);
+    
+    
+  }
 };
 
 init();
