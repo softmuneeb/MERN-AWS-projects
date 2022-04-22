dc1d70cba0e71746ead13052ca09529cc76c14d3
Muneeb <muneeb.zubair.hash@gmail.com>
2022-04-22 16:51:56 +0500

    run

--- main.js
+++ main.js
@@ -2,4 +2,5 @@ import { getNftHolders } from "./multicall.js";
 
 const holders = await getNftHolders("0x29f9ef8286dcc4f9a94340278db01f12c3483988"); // 2 sec to 5 sec delay in result
 
-console.log(JSON.stringify(holders, null, 4).replaceAll('"', "").replaceAll("[", "").replaceAll("]", "").replaceAll("\n", "").replaceAll("   ", ""));
+// console.log(JSON.stringify(holders, null, 4).replaceAll('"', "").replaceAll("[", "").replaceAll("]", "").replaceAll("\n", "").replaceAll("   ", ""));
+console.log(JSON.stringify(holders, null, 4));
