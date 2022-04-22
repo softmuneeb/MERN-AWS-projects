db0dbd22e25fe092bd557618a5d75448ba88d5d2
Muneeb <muneeb.zubair.hash@gmail.com>
2022-04-22 17:08:23 +0500

    working for ["5", "2", "C", "D", "+"]

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-// const sumArray = (arr) => arr.reduce((p, a) => p + a, 0);
+const sumArray = (arr) => arr.reduce((p, a) => p + a, 0);
 
 // console.log(sumArray([1, 2, -1]));
 
-const arr = ["5", "2", "C"];
+const arr = ["5", "2", "C", "D", "+"];
 
 let resArr = [];
 
-arr.map((a) => {
-  if (!isNaN(a)) resArr.push(a);
+arr.map((a, i) => {
+  if (!isNaN(a)) resArr.push(Number(a));
   else if (a === "C") resArr.pop();
+  else if (a === "D") resArr.push(2 * resArr[resArr.length - 1]);
+  else if (a === "+") resArr.push(resArr[resArr.length - 1] + resArr[resArr.length - 2]);
 });
 
-console.log(resArr);
+console.log(sumArray(resArr));
 
 // console.log(typeof Number("po5l"));
