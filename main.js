293183098ef0ec72ca19f0ee7d1c7334a5a12108
Muneeb <muneeb.zubair.hash@gmail.com>
2022-04-25 22:40:08 +0500

    running good on testnet

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ import { nftBuyBot } from './nftBot.js';
 import { minutes } from './utils.js';
 import { PV_KEYS } from './secret.js';
 
-const startAccId = 5,
-  endAccId = 7,
-  waitInSecondsFrom = 1 * minutes,
-  waitInSecondsTo = 2 * minutes;
+const startAccId = 0,
+  endAccId = 4,
+  waitInSecondsFrom = 0.2 * minutes,
+  waitInSecondsTo = 0.4 * minutes;
 
 nftBuyBot(startAccId, endAccId, waitInSecondsFrom, waitInSecondsTo, PV_KEYS);
 
