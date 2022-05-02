0c6ca847d44c2e806ef7188717c0c3811422c10e
Muneeb <muneeb.zubair.hash@gmail.com>
2022-05-02 09:00:20 +0500

    json convert

--- api.js
+++ api.js
@@ -14,20 +14,18 @@ router.get("/metadata/:tokenId", async (req, res) => {
   if (Number(req.params.tokenId) < Number(totalSupply)) {
     const metadata = fs.readFileSync("./metadata/" + req.params.tokenId, "utf8");
     res.send(JSON.parse(metadata));
-  } else res.send(fs.readFileSync("./metadata/pre_reveal", "utf8"));
+  } else {
+    const metadata = fs.readFileSync("./metadata/pre_reveal", "utf8");
+    res.send(JSON.parse(metadata));
+  }
 });
 
-router.get("/is-penguin-available/:tokenId", async (req, res) => {
-  res.send({ success: data.data.success, message: data.data.message });
+router.get("/images/:tokenId", async (req, res) => {
+  res.send(fs.readFileSync("./images/1.jpg"));
 });
 
-router.post("/ninjas", async (req, res, next) => {
-  res.send({ success: data.data.success, message: data.data.message });
-});
-
-router.post("/error", async (req, res, next) => {
-  const data = await axios.post(api + "/ninjas", { metadata: req.body.metadata, image: req.body.image });
-  res.send({ success: data.data.success, message: data.data.message });
-});
+// router.post("/ninjas", async (req, res, next) => {
+//   res.send({ success: data.data.success, message: data.data.message });
+// });
 
 module.exports = router;
