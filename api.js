d993732e65816a632df4a1ebb2744921f2526c4c
Muneeb <muneeb.zubair.hash@gmail.com>
2022-05-02 08:57:14 +0500

    total supply coming from smart contract

--- api.js
+++ api.js
@@ -2,13 +2,16 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
+const { getContractNft } = require("./smart-contracts-config");
 
 router.get("/", async (req, res) => {
   res.send({ message: "Assalamo Alaikum" });
 });
 
 router.get("/metadata/:tokenId", async (req, res) => {
-  if (Number(req.params.tokenId) < 10) {
+  const totalSupply = await getContractNft().methods.totalSupply().call();
+
+  if (Number(req.params.tokenId) < Number(totalSupply)) {
     const metadata = fs.readFileSync("./metadata/" + req.params.tokenId, "utf8");
     res.send(JSON.parse(metadata));
   } else res.send(fs.readFileSync("./metadata/pre_reveal", "utf8"));
