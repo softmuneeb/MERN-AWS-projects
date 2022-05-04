fbdb2025b1dc1754b9deecf3e077120c8aee4fba
Muneeb <muneeb.zubair.hash@gmail.com>
2022-05-04 05:00:22 +0500

    output 2 accounts send eth, 30 sec 2 accounts sent eth

--- main-send-eth.js
+++ main-send-eth.js
@@ -1,6 +1,6 @@
 import { sendEthToAccountsBatch } from "./nftBot.js";
 import web3 from 'web3';
 
-const startAccId = 0, endAccId = 10;
+const startAccId = 0, endAccId = 2;
 
 sendEthToAccountsBatch(startAccId, endAccId, web3.utils.toWei("0.0001", "ether"));
