a2705a86a1cbe9358d49d3604ea4600e0d2e4029
Muneeb <muneeb.zubair.hash@gmail.com>
2022-05-26 14:23:35 +0500

    write plan

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
 // what this code do? 
+// this code helps is giving royalties to nft holders if they sell nft at a good price
+// drawbacks, a person sells to himself, solution, use bird api account rating etc, age, balance, no of tx
 
 // plan
-//
+// get nft trades on a collection
+// token id | mint at eth | sell at eth |    % sold  | reward to give
+//    10    |     1       |     1.5     |     50%    | 100% |
+//    51    |     1       |     1.1     |     10%    | 100% |
+//    17    |     1       |     1.5     |     50%    | 100% | 
+//    21    |     1       |     2.1     |     210%   |
+//    70    |     1       |     1.9     |     90%    |
+// 
+
+// What is the input of the following code?
+// What is the output of the following code?
 
 const init = async () => {
-  // What is the output of the following code?
 
-  let arr = [1, "Turing", { x: 2 }, [3, 4]];
-
-  console.log(arr.length);
-  delete arr[1];
-
-  console.log(arr.length);
-  console.log(arr);
-
-  /*
-4
-4
-[ 1, <1 empty item>, { x: 2 }, [ 3, 4 ] ]
-  */
 };
 
 init();
