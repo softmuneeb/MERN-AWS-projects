a614732cd02189ee1903fd4beeeac75cab33c492
Muneeb <muneeb.softblock@gmail.com>
2022-05-31 12:46:48 +0500

    secret example

---
+++ secret.example.js
@@ -0,0 +1,11 @@
+// mainnet
+const serverUrl = '';
+const appId = '';
+const masterKey = '';
+
+// testnet
+// const serverUrl = '';
+// const appId = '';
+// const masterKey = '';
+
+module.exports = { serverUrl, appId, masterKey };
