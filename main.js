b1d3017fa205da1a11e508009efbe2dcfd8b9aae
Muneeb <muneeb.softblock@gmail.com>
2022-06-10 13:47:02 +0500

    making single complexity flow first

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-import { getNftsStaked } from './multicall.js';
+import { getTokenIdsInStaking, getTokenIdsInStakingOfThisAddress } from './multicall.js';
 
-const holders = await getNftsStaked(); // 2 sec to 5 sec delay in result
+// const holders = await getTokenIdsInStaking(); // 2 sec to 5 sec delay in result
 
 // console.log(JSON.stringify(holders, null, 4).replaceAll('"', "").replaceAll("[", "").replaceAll("]", "").replaceAll("\n", "").replaceAll("   ", ""));
-console.log(JSON.stringify(holders, null, 4));
+// console.log(JSON.stringify(holders, null, 4));
+
+getTokenIdsInStakingOfThisAddress();
--- multicall.js
+++ multicall.js
@@ -1,7 +1,7 @@
 import { Multicall } from "ethereum-multicall";
-import { defaultWeb3, getContractNft, multicallCustomContractAddress, nftAbi, nftAddress, stakingAddress } from "./smart-contract.js";
+import { defaultWeb3, getContractNft, multicallCustomContractAddress, nftAbi, nftAddress, stakingAbi, stakingAddress } from "./smart-contract.js";
 
-export const getNftsStaked = async () => {
+export const getTokenIdsInStaking = async () => {
   const multicall = new Multicall({
     multicallCustomContractAddress,
     web3Instance: defaultWeb3,
@@ -24,6 +24,48 @@ export const getNftsStaked = async () => {
       reference: 'Call1',
       contractAddress: nftAddress,
       abi: nftAbi,
+      calls: calls1,
+    },
+  ];
+
+  const results = await multicall.call(contractCallContext);
+
+  // console.log(JSON.stringify(results));
+
+  let holders = [];
+  results.results['Call1'].callsReturnContext.map((obj, i) => obj.success && holders.push(obj.returnValues[0]));
+
+  // console.log(JSON.stringify(results.results['Call1'].callsReturnContext, null, 4));
+
+  return holders;
+};
+
+export const getTokenIdsInStakingOfThisAddress = async () => {
+  const tokenIdsInStaking = await getTokenIdsInStaking();
+  
+
+  const multicall = new Multicall({
+    multicallCustomContractAddress,
+    web3Instance: defaultWeb3,
+    tryAggregate: true,
+  });
+
+  let calls1 = [];
+
+  let nftStakedTotal = await getContractNft().methods.balanceOf(stakingAddress).call();
+
+  for (let i = 0; i < 150; i++) {
+    calls1.push({
+      methodName: 'nftOwnerOf',
+      methodParameters: [stakingAddress, Number(tokenIdsInStaking[i].hex)],
+    });
+  }
+
+  const contractCallContext = [
+    {
+      reference: 'Call1',
+      contractAddress: stakingAddress,
+      abi: stakingAbi,
       calls: calls1,
     },
   ];
