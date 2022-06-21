12a2b33ad89d3c20022b5d0c362ccfbcdb73fa89
Muneeb <muneeb.softblock@gmail.com>
2022-06-21 15:53:59 +0500

    code

--- main.js
+++ main.js
@@ -3,22 +3,5 @@
 // plan
 //
 
-const init = async () => {
-  // What is the output of the following code?
 
-  let arr = [1, "Turing", { x: 2 }, [3, 4]];
-
-  console.log(arr.length);
-  delete arr[1];
-
-  console.log(arr.length);
-  console.log(arr);
-
-  /*
-4
-4
-[ 1, <1 empty item>, { x: 2 }, [ 3, 4 ] ]
-  */
-};
-
-init();
+// code
