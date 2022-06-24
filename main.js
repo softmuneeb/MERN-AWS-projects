570561400cb91196917e1da867016a50996b12c3
Muneeb <muneeb.softblock@gmail.com>
2022-06-24 17:07:38 +0500

    moralis x heroku x cloudflare refersh

--- main.js
+++ main.js
@@ -1,30 +1,14 @@
 // what this code do?
-// this code helps is giving royalties to nft holders if they sell nft at a good price
-// drawbacks, a person sells to himself, solution, use bird api account rating etc, age, balance, no of tx
-
-// plan
-// get nft trades on a collection
-// token id | mint at eth | sell at eth |    % sold  | reward to give
-//    10    |     1       |     1.5     |     50%    | 100% |
-//    51    |     1       |     1.1     |     10%    | 100% |
-//    17    |     1       |     1.5     |     50%    | 100% |
-//    21    |     1       |     2.1     |     210%   |
-//    70    |     1       |     1.9     |     90%    |
-//
-
-// What is the input of the following code?
-// What is the output of the following code?
+// get nft trades from opensea using
 
 const Moralis = require('moralis/node');
-const { serverUrl, appId, masterKey } = require('./secret2.js');
+const { serverUrl, appId, masterKey } = require('./secret.js');
 
 const init = async () => {
   await Moralis.start({ serverUrl, appId, masterKey });
-
-  const options = { address: '0x350b4cdd07cc5836e30086b993d27983465ec014', limit: '3', chain: 'eth' };
+  const options = { address: '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D', limit: '1', chain: 'eth' };
   const NFTTrades = await Moralis.Web3API.token.getNFTTrades(options);
-
-  console.log(JSON.stringify(NFTTrades));
+  console.log('Just to refresh moralis server, daily, ' + JSON.stringify(NFTTrades, null, 4));
 };
 
 init();
---
+++ secret.js
@@ -0,0 +1,11 @@
+// mainnet
+const serverUrl = 'https://fhm1t1xuva0b.usemoralis.com:2053/server';
+const applicationId = 'Scwz6t8lHO5KmHky2uL6k4kOnBlPpZq4iOaGmZtU';
+const masterKey = 'r7qweHnP7fkWRjzGtVw1Gw1TjGJ7PboO9bi1rTkj';
+
+// testnet
+// const serverUrl = '';
+// const appId = '';
+// const masterKey = '';
+
+module.exports = { serverUrl, applicationId, masterKey };
