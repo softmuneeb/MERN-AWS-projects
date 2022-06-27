48742f94af4224890cb670b750cc5b3b6ffd16a7
Muneeb <muneeb.softblock@gmail.com>
2022-06-28 01:40:36 +0500

    auto push

--- main.js
+++ main.js
@@ -4,5 +4,5 @@
 const execSync = require('child_process').execSync;
 // import { execSync } from 'child_process';  // replace ^ if using ES modules
 
-const output = execSync("git add . && git commit -am 'auto push' && git push --set-upstream origin terminal commands", { encoding: 'utf-8' }); // the default is 'buffer'
+const output = execSync("git add . && git commit -am 'auto push' && git push --set-upstream origin terminal-commands", { encoding: 'utf-8' }); // the default is 'buffer'
 console.log('Output was:\n', output);
