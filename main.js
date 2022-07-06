07fd04a8f0938ceee0f0a4f41b493540bf925b34
Muneeb <muneeb.softblock@gmail.com>
2022-07-06 11:14:26 +0500

    4 parallel bots

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ import { nftBuyBot } from './nftBot.js';
 import { minutes } from './utils.js';
 import { PV_KEYS } from './secret.js';
 import { PV_KEYS2 } from './secret2.js';
+import { PV_KEYS3 } from './secret3.js';
+import { PV_KEYS4 } from './secret4.js';
 
 let numOfAccountsToUse = 1;
 
@@ -10,3 +12,5 @@ let accIdToUse = Number('' + fs.readFileSync('accIdToUse'));
 
 nftBuyBot(PV_KEYS, accIdToUse, numOfAccountsToUse, 0 * minutes, 0 * minutes);
 nftBuyBot(PV_KEYS2, accIdToUse, numOfAccountsToUse, 0 * minutes, 0 * minutes);
+nftBuyBot(PV_KEYS3, accIdToUse, numOfAccountsToUse, 0 * minutes, 0 * minutes);
+nftBuyBot(PV_KEYS4, accIdToUse, numOfAccountsToUse, 0 * minutes, 0 * minutes);
