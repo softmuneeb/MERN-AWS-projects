2899b9b887e02bf427ed331942d230b32011356a
Muneeb <muneeb.softblock@gmail.com>
2022-07-08 09:00:04 +0500

    starter

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
+const fs = require("fs");
 const HTMLtoDOCX = require("html-to-docx");
 
 const init = async () => {
-  const a = await HTMLtoDOCX(
-    '<html><body><divclass="container"><h1>404</h1></div></body></html>'
-  );
-  const fs = require("fs");
-  await fs.writeFile("filename1.docx", a);
-  console.log("a: ", a);
+  const a = await HTMLtoDOCX('<html><body>hi</body></html>');
+  await fs.writeFileSync("aoa.docx", a);
 };
 
 init();
