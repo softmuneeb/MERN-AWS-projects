4b27e2baa6c894712cdc7f12fb27247acc4ac034
Muneeb <muneeb.softblock@gmail.com>
2022-07-08 07:44:49 +0500

    okay

--- refreshMetadata.js
+++ refreshMetadata.js
@@ -1,21 +1,9 @@
 // dependencies
-const axios = require('axios');
+import pkg from 'axios';
+const { get } = pkg;
 
-// config
-const nft = '0x1d4f5FBC869fDefd2b6D0e20C9c679E03E4CFF2F';
-const tokenIdFrom = 108;
-const tokenIdTo = 132;
+const url = `https://www.google.com/`;
 
-// run code
-const init = async () => {
-  for (let i = tokenIdFrom; i <= tokenIdTo; i++) {
-    const url = `https://api.opensea.io/api/v1/asset/${nft}/${i}/?force_update=true`;
-    console.log(`url: ${url}`);
-    const res = await axios.get(url);
+const res = await get(url);
 
-    if (res.data.image_url.length < 5) console.log('not ', i);
-    else if (i % 1 === 0) console.log('ok ', i);
-  }
-};
-
-init();
+console.log(res.data);
