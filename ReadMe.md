695ff780054a974c76125e9762473a3e3b5eae17
Muneeb <muneeb.softblock@gmail.com>
2022-07-15 19:27:13 +0500

    success get nft tarde
    [
      {
        from: '0x2ae24e0EF11169676f6Bc58828e0a9220d0368d3',
        value: '12000000000000000',
        tokenIds: [ '925', '6493' ]
      }
    ]

---
+++ ReadMe.md
@@ -0,0 +1,12 @@
+
+
+todos small improvemets:
+* getTransaction vs getTransactionReceipt speed comparison
+* getTransaction vs getTransactionReceipt ERC20 token trades detect
+
+ideas:
+* loopholes, if someone sells nft a higher cost to his own wallet?
+
+
+store:
+console.log({ value, from, tokenId });
---
+++ getNftTrades.js
@@ -0,0 +1,70 @@
+/*
+
+Interface
+
+const getNftTrades = (fromBlock, toBlock) => {
+  // ignore 0 value trades
+
+  return [
+    {
+      value: '1000000',
+      from: '0xae2765784425F7ad12de01A064080E578E3da8AA',
+      tokenIds: ['825', '1', '2', 3],
+    },
+    {
+      value: '20000000',
+      from: '0xae2765784425F7ad12de01A064080E578E3da8AA',
+      tokenIds: ['9'],
+    }
+  ];
+};
+
+*/
+
+import Web3 from 'web3';
+
+const getEventsWithCombinedTokenIdsInSingleSale = async (web3, events) => {
+  let txs = {};
+
+  for (let i = 0; i < events.length; i++) {
+    const event = events[i];
+
+    const { from, tokenId, transactionHash } = event.returnValues;
+
+    const { value } = await web3.eth.getTransaction(event.transactionHash);
+
+    const tokenIds = txs[transactionHash] ? [tokenId, ...txs[transactionHash].tokenIds] : [tokenId];
+
+    txs[transactionHash] = { from, value, tokenIds };
+  }
+
+  const combinedTokenIdEvents = Object.keys(txs).map((tx) => txs[tx]);
+
+  return combinedTokenIdEvents;
+};
+
+const getEventsWithoutMint = (events) => events.filter((event) => event.returnValues.from !== '0x0000000000000000000000000000000000000000');
+
+const getTransferEvents = async (web3, NFT_ABI, NFT_ADDRESS, fromBlock, toBlock) => {
+  const eventsAll = await new web3.eth.Contract(NFT_ABI, NFT_ADDRESS).getPastEvents('Transfer', {
+    fromBlock,
+    toBlock,
+  });
+
+  return eventsAll;
+};
+
+export const getNftTrades = async (BLOCKCHAIN_URL, NFT_ABI, NFT_ADDRESS, fromBlock, toBlock) => {
+  const web3 = new Web3(BLOCKCHAIN_URL);
+
+  const events = await getTransferEvents(web3, NFT_ABI, NFT_ADDRESS, fromBlock, toBlock);
+
+  const eventsWithoutMints = getEventsWithoutMint(events);
+  const eventsWithCombinedTokenIdsInSingleSale = getEventsWithCombinedTokenIdsInSingleSale(web3, eventsWithoutMints);
+  return eventsWithCombinedTokenIdsInSingleSale;
+
+  // get transfer events
+  // for every event
+  //  get tx hash and find its price (value) in ETH paid in that transaction
+  // send that list back to earth
+};
