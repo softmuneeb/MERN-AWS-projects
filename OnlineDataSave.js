5cb19b954ba179382982d13eec65d7d4cd879af8
Muneeb <muneeb.softblock@gmail.com>
2022-07-18 12:27:26 +0500

    to improve later

--- OnlineDataSave.js
+++ OnlineDataSave.js
@@ -8,20 +8,22 @@ const TodoSchema = new mongoose.Schema({
   date: { type: Number, default: new Date() },
 });
 
-const Todo = new mongoose.model('TodoModel', TodoSchema);
-
 const readRoyaltySettings = async () => {
+  const Todo = new mongoose.model('TodoModel', TodoSchema);
+
   mongoose.connect('mongodb+srv://User123:pakistan0047@verysmallcluster.gq04lby.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
   const response = await Todo.find({ _id: '62d4f26ae0242c27a4e39ebf' });
   await mongoose.connection.close();
-  console.log({ response: response[0].record });
+
+  if (response.length === 0) return {};
   return response[0].record;
 };
 
 const saveRoyaltySettings = async (record) => {
+  const Todo = new mongoose.model('TodoModel', TodoSchema);
   mongoose.connect('mongodb+srv://User123:pakistan0047@verysmallcluster.gq04lby.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -29,11 +31,14 @@ const saveRoyaltySettings = async (record) => {
   const response = await Todo.updateOne({ _id: '62d4f26ae0242c27a4e39ebf' }, { record });
   await mongoose.connection.close();
 
-  console.log({ response });
   return response;
 };
 
-// saveRoyaltySettings({});
-// readRoyaltySettings();
+// (async () => {
+//   // const data = await saveRoyaltySettings({ 1: 1 });
+//   // console.log({ data });
+//   // const data = await readRoyaltySettings();
+//   // console.log({ data });
+// })();
 
-module.exports = { readRoyaltySettings, saveRoyaltySettings };
+// module.exports = { readRoyaltySettings, saveRoyaltySettings };
