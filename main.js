1e256355a77bdc06a83baec85cc19140e079c374
Muneeb <muneeb.softblock@gmail.com>
2022-08-27 03:08:03 +0500

    working al hamdu lillah next try on other networks so open sea shows trades, https://testnets.opensea.io/assets/rinkeby/0x05fe4727d3ec1280b2224602cfe7b0c866526c33/1

--- main.js
+++ main.js
@@ -1,5 +1,51 @@
-// what this code do? 
+// Listing an ERC-721 for 10 ETH and fulfilling it https://packagegalaxy.com/javascript/@opensea/seaport-js
 
-// plan, steps to do
+import pkg from '@opensea/seaport-js';
+const { Seaport } = pkg;
+import { ethers } from 'ethers';
 
-// code
+const provider = new ethers.providers.JsonRpcProvider('https://eth-rinkeby.alchemyapi.io/v2/nbSKzaIzs5qSfrrjPnDSmziHeKQXQNhz');
+
+const PB1 = '0xa827c2964536668d9d2ce10962392c328af4c131';
+const PV1 = 'e88d627a33fb2514f8933550fec8fceead9fe6a0a08387011acc26eb48701828';
+
+const PB2 = '0xa286ae2E1A0e199Bd89731efDb2d6162e23825b9';
+const PV2 = 'fe4c05f39bb90a57ef97338dd231339cdcc4fadbfaee3b08bd4c401da5ce8b5e';
+
+const offerer = PB1;
+const fulfiller = PB2;
+const signer = new ethers.Wallet(PV1, provider);
+
+const seaport = new Seaport(signer);
+
+const { executeAllActions } = await seaport.createOrder(
+  {
+    offer: [
+      {
+        itemType: 2, //@opensea/seaport-js/lib/constants.js
+        token: '0x05fe4727d3ec1280b2224602cfe7b0c866526c33',
+        identifier: '1',
+      },
+    ],
+    consideration: [
+      {
+        amount: '100',
+        recipient: offerer,
+      },
+    ],
+  },
+  offerer,
+);
+
+const order = await executeAllActions();
+
+console.log({ order });
+
+const { executeAllActions: executeAllFulfillActions } = await seaport.fulfillOrder({
+  order,
+  accountAddress: fulfiller,
+});
+
+const transaction = await executeAllFulfillActions();
+
+console.log({ transaction });
