81b7625fe8dcbc9c45caf5e975fccc6ff904d14a
Muneeb <muneeb.softblock@gmail.com>
2022-08-30 22:32:43 +0500

    try in windows

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 // what this code do? plan, steps to do
+
+const { Builder, Browser, By, Key, until } = require('selenium-webdriver');
+
+(async function example() {
+  let driver = await new Builder().forBrowser(Browser.CHROME).build();
+  try {
+    await driver.get('http://www.google.com');
+    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    // await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+  } finally {
+    await driver.quit();
+  }
+})();
