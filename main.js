a1a1ebd6d964916b136f725eb111b8583a24b90d
Muneeb <muneeb.softblock@gmail.com>
2022-11-17 04:19:51 +0500

    basic ok
    $ node main.js
    [
      [ 9, 9, 9 ],
      [ 9, 9, 9 ],
      [ 9, 9, 9 ],
      [ 0, 0, 0 ],
      [ 0, 0, 0 ],
      [ 0, 0, 1 ],
      [ 0, 1, 1 ],
      [ 1, 1, 1 ]
    ]
    { sol: 1 }

--- main.js
+++ main.js
@@ -1,1 +1,120 @@
 // what this code do? plan, steps to do
+
+// 55MINS..
+
+// parts
+// DONE, fell figure down to height x steps
+// fell only if found space
+// fell properly, 0 covers 1 is not correct, merge the 2 rows
+// check any full rows available
+// refactor complexity 1 of code, show adil, ...
+const emptySpaceFound = (a, b) => {
+  return 0 === a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+};
+const merge = (a, b) => {
+  return [a[0] + b[0], a[1] + b[1], a[2] + b[2]];
+};
+const allFilled = (a) => {
+  return 1 === a[0] * a[1] * a[2];
+};
+function solution(field, figure) {
+  let rowFallen = 0;
+  for (let i = 0; i < field.length; i++) {
+    if (emptySpaceFound(field[i], [...figure[figure.length - 1]])) {
+      rowFallen = 1;
+
+      if (i - 3 >= 0) field[i - 3] = [0, 0, 0];
+      if (i - 2 >= 0) field[i - 2] = [...figure[figure.length - 3]];
+      if (i - 1 >= 0) field[i - 1] = [...figure[figure.length - 2]];
+      field[i] = merge(field[i], [...figure[figure.length - 1]]); // field[next] = figure[last]
+    } else {
+      break;
+    }
+  }
+
+  console.log([...zeros, ...field]);
+
+  for (let i = 0; i < field.length; i++) {
+    if (allFilled(field[i])) return rowFallen;
+  }
+
+  return -1;
+  //   field[0 + 0] = [...figure[figure.length - 1]];
+
+  //   field[0 + 0] = [...figure[figure.length - 2]];
+  //   field[0 + 1] = [...figure[figure.length - 1]]; // field[next] = figure[last]
+
+  //   field[0 + 0] = [...figure[figure.length - 3]];
+  //   field[0 + 1] = [...figure[figure.length - 2]]; // field[next] = figure[last]
+  //   field[0 + 2] = [...figure[figure.length - 1]]; // field[next] = figure[last]
+
+  //   field[0 + 0] = [0, 0, 0];
+  //   field[0 + 1] = [...figure[figure.length - 3]];
+  //   field[0 + 2] = [...figure[figure.length - 2]]; // field[next] = figure[last]
+  //   field[0 + 3] = [...figure[figure.length - 1]]; // field[next] = figure[last]
+
+  //   field[0 + 1] = [0, 0, 0];
+  //   field[0 + 2] = [...figure[figure.length - 3]];
+  //   field[0 + 3] = [...figure[figure.length - 2]]; // field[next] = figure[last]
+  //   field[0 + 4] = [...figure[figure.length - 1]]; // field[next] = figure[last]
+
+  //   field[0] = [...figure[2]];
+
+  //   field[0] = [...figure[2]];
+  //   field[1] = [...figure[2]];
+
+  //   field = [...figure, ...field];
+
+  // 0 - 4 = height of field
+  //   const heightOfField = field.length;
+  //   for (let i = 0; i < heightOfField; i++) {
+  //     if (emptySpaceFound(field[3 + i], field[2 + i])) {
+  //       field[3 + i] = merge(field[3 + i], field[2 + i]);
+  //       field[2 + i] = field[1 + i];
+  //       field[1 + i] = field[0 + i];
+  //       field[0 + i] = [0, 0, 0];
+  //     } else {
+  //       break;
+  //     }
+  //   }
+
+  //   field[3 + 1] = field[2 + 1];
+  //   field[2 + 1] = field[1 + 1];
+  //   field[1 + 1] = field[0 + 1];
+  //   field[0 + 1] = [0, 0, 0];
+
+  //   field[3 + 2] = field[2 + 2];
+  //   field[2 + 2] = field[1 + 2];
+  //   field[1 + 2] = field[0 + 2];
+  //   field[0 + 2] = [0, 0, 0];
+
+  //   field[3 + 3] = field[2 + 3];
+  //   field[2 + 3] = field[1 + 3];
+  //   field[1 + 3] = field[0 + 3];
+  //   field[0 + 3] = [0, 0, 0];
+
+  //   field[3 + 4] = field[2 + 4];
+  //   field[2 + 4] = field[1 + 4];
+  //   field[1 + 4] = field[0 + 4];
+  //   field[0 + 4] = [0, 0, 0];
+}
+
+const zeros = [
+  [9, 9, 9],
+  [9, 9, 9],
+  [9, 9, 9],
+];
+const figure = [
+  [0, 0, 1],
+  [0, 1, 1],
+  [0, 0, 1],
+];
+const field = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+  [1, 0, 0],
+  [1, 1, 0],
+];
+
+console.log({ sol: solution(field, figure) });
