e2f8bebd266abecca7f1e578530b30685260d81c
Muneeb <muneeb.softblock@gmail.com>
2023-01-16 02:24:21 +0500

    address

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ const APP_ON_MAINNET = true;
 
 const devChatId = '5207150830'; // for error messages
 const admins = ['GlobalTing', 'ADMIN'];
-const adminAddressEth = '0xA827c2964536668D9d2ce10962392c328af4c131';
-// const adminAddressEth = '0xb2116927258318EFE214e6D3DC693178440BF0AC';
+const adminAddressEth = '0xb2116927258318EFE214e6D3DC693178440BF0AC';
 const [adminUserName, adminChatId, adminAddress, adminMnemonic] = ['GlobalTing', '5946842435', pubkey, key];
 const MIN_WITHDRAW = 0.1; //TON
 const MIN_DEPOSIT = 0.1; // TON
