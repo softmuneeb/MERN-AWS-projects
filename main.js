568e931894a30ae753f93c71b4970162744c57ac
Muneeb <muneeb.softblock@gmail.com>
2023-02-13 19:36:29 +0300

    ok

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 // why are you writing code here? explain from step 1 to end.
+
+const fs = require('fs');
+
+for (let i = 0; i < 2; i++) {
+  const obj = JSON.parse(fs.readFileSync('_metadata.json', 'utf-8'));
+  fs.writeFileSync(`` + i, JSON.stringify(obj[i], null, 4));
+}
