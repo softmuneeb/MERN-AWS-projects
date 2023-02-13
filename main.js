b3f93af2afff3029b1a2c8d95901fde0af237172
Adilkhan012 <khanadil123457@gmail.com>
2023-02-14 02:55:08 +0500

    meta done

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const obj = JSON.parse(fs.readFileSync('_metadata.json', 'utf-8'));
 
 // https://nftstorage.link/ipfs/bafybeid222qqmcbznk7wp7jugbkpjd5unwzlvu6bqoq36e4onusuvagybi/1.png
 
-for (let i = 0; i < (3 || obj.length); i++) {
-  obj[i].image = '/' + (i + 1) + '.png';
+for (let i = 0; i < (10000 || obj.length); i++) {
+  obj[i].image = 'https://nftstorage.link/ipfs/bafybeid222qqmcbznk7wp7jugbkpjd5unwzlvu6bqoq36e4onusuvagybi/' + (i + 1) + '.png';
   fs.writeFileSync(`meta/` + (i + 1), JSON.stringify(obj[i], null, 4));
 }
