5239b2d88e72b6ab59e092bee0b570698f77ae2b
Muneeb <muneeb.softblock@gmail.com>
2023-02-17 03:24:00 +0300

    test

--- balances.js
+++ balances.js
@@ -11,14 +11,17 @@ export const balances = async () => {
   const ethereum = new wallet({
     numberOfAddresses: 100,
     mnemonic: SERVICE_WALLETS_MNEMONIC,
+    // 'laundry fine crew require boss spy chuckle illness donate lion arm juice trigger alarm this wine steel repair pistol bus course mesh maple marriage',
     providerOrUrl: networkLinks[0],
     pollingInterval: 86400 * 20 * 1000, // this lines saves extra calls to infura or rpc end point
   });
   const web3 = new Web3(ethereum);
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 5; i++) {
     const user = ethereum.getAddress(i);
     const balance = await web3.eth.getBalance(user);
     console.log(`${user} -> ${Web3.utils.fromWei(balance)} MATIC`);
   }
   ethereum.engine.stop();
 };
+
+// balances()
