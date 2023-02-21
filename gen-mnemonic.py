e6dc9965c21c7b3705e5cecebddbbf629c4a34b3
Muneeb <muneeb.softblock@gmail.com>
2023-02-22 01:58:02 +0300

    ok

--- gen-mnemonic.py
+++ gen-mnemonic.py
@@ -1,4 +1,12 @@
 from mnemonic import Mnemonic
+from web3 import Web3
+w3 = Web3()
+
 mnemo = Mnemonic("english")
 words = mnemo.generate(strength=256)
 print(words)
+
+w3.eth.account.enable_unaudited_hdwallet_features()
+account = w3.eth.account.from_mnemonic(words, account_path="m/44'/60'/0'/0/0")
+print(account.address)
+print(account.key)
