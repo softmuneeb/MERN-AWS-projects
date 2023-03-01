67d56c7354124c789579f9725faf1b3fa55a00ef
Muneeb <muneeb.softblock@gmail.com>
2023-03-01 19:33:48 +0300

    metadata okay

--- api.js
+++ api.js
@@ -1,36 +1,43 @@
 // dependencies
-const fs = require("fs");
-const express = require("express");
-const router = express.Router();
+const fs = require('fs')
+const express = require('express')
+const router = express.Router()
 
-const { getContractNft } = require("./smart-contracts-config");
+const { getContractNft } = require('./smart-contracts-config')
 
-router.get("/", async (req, res) => {
-  res.send({ message: "Assalamo Alaikum" });
-});
+router.get('/', async (req, res) => {
+  res.send({ message: 'Assalamo Alaikum' })
+})
 
-router.get("/metadata/:tokenId", async (req, res) => {
-  const totalSupply = await getContractNft().methods.totalSupply().call();
+router.get('/metadata/:tokenId', async (req, res) => {
+  const tokenId = Number(req.params.tokenId)
+  let domainName
+  try {
+    domainName = await getContractNft()
+      .methods.domainNameOf(tokenId - 1)
+      .call()
+  } catch (e) {
+    res.send({})
+    return
+  }
+
+  const metadata = {
+    name: domainName,
+    image: `https://localhost:4000/api/images/${domainName}`,
+  }
+  res.send(metadata)
+})
+
+router.get('/images/:tokenId', async (req, res) => {
+  const totalSupply = await getContractNft().methods.totalSupply().call()
 
   if (Number(req.params.tokenId) < Number(totalSupply)) {
-    const metadata = fs.readFileSync("./metadata/" + req.params.tokenId, "utf8");
-    res.send(JSON.parse(metadata));
+    const imagePath = __dirname + '/images/' + req.params.tokenId + '.jpg'
+    res.sendFile(imagePath)
   } else {
-    const metadata = fs.readFileSync("./metadata/pre_reveal", "utf8");
-    res.send(JSON.parse(metadata));
+    const imagePath = __dirname + '/images/pre_reveal.jpeg'
+    res.sendFile(imagePath)
   }
-});
+})
 
-router.get("/images/:tokenId", async (req, res) => {
-  const totalSupply = await getContractNft().methods.totalSupply().call();
-
-  if (Number(req.params.tokenId) < Number(totalSupply)) {
-    const imagePath = __dirname + "/images/" + req.params.tokenId + ".jpg";
-    res.sendFile(imagePath);
-  } else {
-    const imagePath = __dirname + "/images/pre_reveal.jpeg";
-    res.sendFile(imagePath);
-  }
-});
-
-module.exports = router;
+module.exports = router
