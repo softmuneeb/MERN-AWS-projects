2008ed7ae74cb516f39267c0ffb469f54b7f5659
Muneeb <muneeb.softblock@gmail.com>
2023-03-01 19:48:22 +0300

    format

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const router = require('./api.js');
-const cors = require('cors');
-const app = express();
-require("dotenv").config();
+const express = require('express')
+const router = require('./api.js')
+const cors = require('cors')
+const app = express()
+require('dotenv').config()
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000
 
-app.use(express.json({ limit: '2mb' }));
-app.use(express.urlencoded({ limit: '2mb' }));
-app.use(cors());
-app.use(express.json());
-app.use('/api', router);
+app.use(express.json({ limit: '2mb' }))
+app.use(express.urlencoded({ limit: '2mb' }))
+app.use(cors())
+app.use(express.json())
+app.use('/api', router)
 
 app.listen(port, () => {
-  console.log('port', port);
-  console.log('Assalamo Alaikum, Muneeb bro');
-});
+  console.log('port', port)
+  console.log('Assalamo Alaikum, Muneeb bro')
+})
